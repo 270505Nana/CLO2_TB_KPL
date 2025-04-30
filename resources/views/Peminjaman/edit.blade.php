c0aeb2945bd775f9038d43690b558a1764a68491
TriMylani <trimylani5@gmail.com>
2025-04-30 18:33:49 +0700

    peminjaman

--- resources/views/Peminjaman/edit.blade.php
+++ resources/views/Peminjaman/edit.blade.php
@@ -1,143 +1,24 @@
-<!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="apple-touch-icon" sizes="76x76" href={{ asset('assets/img/brain.png') }}">
-    <link rel="icon" type="image/png" href="{{ asset('assets/img/brain.png') }}">
-    <title>
-        Manajemen Perpustakaan
-    </title>
-    <!--     Fonts and icons     -->
+    <title>@yield('title', 'Peminjaman')</title>
+
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
-    <!-- Nucleo Icons -->
     <link href="https://demos.creative-tim.com/argon-dashboard-pro/assets/css/nucleo-icons.css" rel="stylesheet" />
     <link href="https://demos.creative-tim.com/argon-dashboard-pro/assets/css/nucleo-svg.css" rel="stylesheet" />
-    <!-- Font Awesome Icons -->
     <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
-    <!-- CSS Files -->
     <link id="pagestyle" href={{ asset('assets/css/argon-dashboard.css?v=2.1.0') }} rel="stylesheet" />
 </head>
 
 <body class="g-sidenav-show bg-gray-100">
     <div class="min-height-300 bg-primary position-absolute w-100"></div>
-    <aside
-        class="sidenav bg-white navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-4 "
-        id="sidenav-main">
-        <div class="sidenav-header">
-            <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
-                aria-hidden="true" id="iconSidenav"></i>
-            <a class="navbar-brand m-0" href=" https://demos.creative-tim.com/argon-dashboard/pages/dashboard.html "
-                target="_blank">
-
-                <img src="{{ asset('assets/img/brain-bg.png') }}" width="35px" height="35px"
-                    class="navbar-brand-img h-100" alt="main_logo">
-                <span class="ms-1 font-weight-bold">Manajemen Perpustakaan</span>
-            </a>
-        </div>
-        <hr class="horizontal dark mt-0">
-
-        {{-- NAVBAR --}}
-        {{-- navbar nnti dibikin full --}}
-        <div class="collapse navbar-collapse  w-auto " id="sidenav-collapse-main">
-            <ul class="navbar-nav">
-
-                {{-- list navbar item --}}
-                {{-- list navbar item --}}
-                <li class="nav-item">
-                    <a class="nav-link active" href="/">
-                        <div
-                            class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-                            <i class="ni ni-tv-2 text-dark text-sm opacity-10"></i>
-                        </div>
-                        <span class="nav-link-text ms-1">Dashboard</span>
-                    </a>
-                </li>
-
-                <li class="nav-item">
-                    {{-- HREF DIUBAH! --}}
-                    <a class="nav-link " href="/databuku">
-                        <div
-                            class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-                            <i class="ni ni-credit-card text-dark text-sm opacity-10"></i>
-                        </div>
-                        <span class="nav-link-text ms-1">Data Buku</span>
-                    </a>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link " href="/datamahasiswa">
-                        <div
-                            class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-                            <i class="ni ni-credit-card text-dark text-sm opacity-10"></i>
-                        </div>
-                        <span class="nav-link-text ms-1">Data Mahasiswa</span>
-                    </a>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link " href="/datapeminjaman">
-                        <div
-                            class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-                            <i class="ni ni-app text-dark text-sm opacity-10"></i>
-                        </div>
-                        <span class="nav-link-text ms-1">Data Peminjaman</span>
-                    </a>
-                </li>
-            </ul>
-        </div>
-    </aside>
+    @include('layouts.sidebar')
 
     <div class="main-content position-relative max-height-vh-100 h-100">
-        <!-- Navbar -->
-        <nav
-            class="navbar navbar-main navbar-expand-lg bg-transparent shadow-none position-absolute px-4 w-100 z-index-2 mt-n11">
-            <div class="container-fluid py-1">
-                <nav aria-label="breadcrumb">
-                    <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 ps-2 me-sm-6 me-5">
-                        <li class="breadcrumb-item text-sm"><a class="text-white opacity-5"
-                                href="javascript:;">Pages</a></li>
-                        <li class="breadcrumb-item text-sm text-white active" aria-current="page">Profile</li>
-                    </ol>
-                    <h6 class="text-white font-weight-bolder ms-2">Profile</h6>
-                </nav>
-                <div class="collapse navbar-collapse me-md-0 me-sm-4 mt-sm-0 mt-2" id="navbar">
-                    <div class="ms-md-auto pe-md-3 d-flex align-items-center">
-                        <div class="input-group">
-                            <span class="input-group-text text-body"><i class="fas fa-search"
-                                    aria-hidden="true"></i></span>
-                            <input type="text" class="form-control" placeholder="Type here...">
-                        </div>
-                    </div>
-                    <ul class="navbar-nav justify-content-end">
-                        <li class="nav-item d-flex align-items-center">
-                            <a href="javascript:;" class="nav-link text-white font-weight-bold px-0">
-                                <i class="fa fa-user me-sm-1"></i>
-                                <span class="d-sm-inline d-none">Sign In</span>
-                            </a>
-                        </li>
-                        <li class="nav-item d-xl-none ps-3 pe-0 d-flex align-items-center">
-                            <a href="javascript:;" class="nav-link text-white p-0">
-                                <a href="javascript:;" class="nav-link text-white p-0" id="iconNavbarSidenav">
-                                    <div class="sidenav-toggler-inner">
-                                        <i class="sidenav-toggler-line bg-white"></i>
-                                        <i class="sidenav-toggler-line bg-white"></i>
-                                        <i class="sidenav-toggler-line bg-white"></i>
-                                    </div>
-                                </a>
-                            </a>
-                        </li>
-                        <li class="nav-item px-3 d-flex align-items-center">
-                            <a href="javascript:;" class="nav-link text-white p-0">
-                                <i class="fa fa-cog fixed-plugin-button-nav cursor-pointer"></i>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        <!-- End Navbar -->
+    @include('layouts.sidebar')
+    
+    <!-- End Navbar -->
         <div class="card shadow-lg mx-4 card-profile-top mt-4">
             <div class="card-body p-3">
                 <div class="row gx-5">
@@ -234,16 +115,15 @@
                 </div>
             </div>
         </div>
+    {{-- penggunaan code reuse --}}
+    @include('layouts.footer')
 
-        </main>
+    <!-- Include SweetAlert2 -->
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src={{ asset('assets/js/argon-dashboard.min.js?v=2.1.0') }}></script>
 
-        <!-- Core JS Files -->
-        <script src="{{ asset('assets/js/core/popper.min.js') }}"></script>
-        <script src="{{ asset('assets/js/core/bootstrap.min.js') }}"></script>
-        <script src="{{ asset('assets/js/plugins/perfect-scrollbar.min.js') }}"></script>
-        <script src="{{ asset('assets/js/plugins/smooth-scrollbar.min.js') }}"></script>
-        <script src="{{ asset('assets/js/argon-dashboard.min.js?v=2.1.0') }}"></script>
-        <script>
+    <script>
+
             // Mengambil form peminjaman dan menangani submit
             document.getElementById('form-peminjaman').addEventListener('submit', async function(e) {
                 e.preventDefault();

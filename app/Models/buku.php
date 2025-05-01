2fd69bc19d72ed70f84e048bef5554cfa8b24f19
MartryatusSofia <martryatuss@gmail.com>
2025-05-01 15:07:57 +0700

    Menambahkan Code reuse di bagian form peminjaman dan mahasiswa

--- app/Models/buku.php
+++ app/Models/buku.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 use Illuminate\Database\Eloquent\Model;
 
-class buku extends Model
+class Buku extends Model
 {
     Use HasFactory;
     protected $fillable = [
